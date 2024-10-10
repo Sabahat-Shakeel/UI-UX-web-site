420aa581359b1623b10485f260ef4d5af23a333b
syeda <sbtshakeel61@gmail.com>
2024-10-10 18:08:46 +0500

    adding code

--- src/app/components/ps/page.tsx
+++ src/app/components/ps/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 export default function Ps() {
   return (
-    <main className=" bg-gradient-to-br to-[#fdfcdc] via-[#e9d8a6] from-[#f4d58d] h-screen ">
+    <main className=" py-5 bg-gradient-to-br to-[#fdfcdc] via-[#e9d8a6] from-[#f4d58d] h-screen ">
       <h1 className=" flex justify-center text-wrap font-bold text-orange-400 text-5xl font-[Noto]">
         {" "}
         All Acessories
@@ -18,36 +18,36 @@ export default function Ps() {
       </div>
       <hr />
       <div
-        className="bg-[#ffccd5] h-screen flex space-x-2 space-y-7 flex-wrap  
+        className="bg-[#ffced7] h-screen flex space-x-2 space-y-7 flex-wrap  
 
 justify-around"
       >
-        <div className="w-40 h-40 sm:w-80 sm:h-80 border border-pink-300  rounded-xl bg-[#fce9ed] ml-1 ">
+        <div className="w-40 h-40 sm:w-80 sm:h-80 border border-pink-300  rounded-xl bg-[#ffe4ea] ml-1 mt-7 ">
           <Image src="/p1.png" alt="pink shoes" width="500" height="500" />
           <span>$500</span>
         </div>
 
-        <div className="w-40 h-40 sm:w-80 sm:h-80 border border-pink-300  rounded-xl bg-[#fce9ed] ml-1">
+        <div className="w-40 h-40 sm:w-80 sm:h-80 border border-pink-300  rounded-xl bg-[#ffe4ea] ml-1">
           <Image src="/p2.png" alt="pink shoes" width="500" height="500" />
           <span>$500</span>
         </div>
 
-        <div className="w-40 h-40 sm:w-80 sm:h-80 border border-pink-300  rounded-xl bg-[#fce9ed] ml-1">
+        <div className="w-40 h-40 sm:w-80 sm:h-80 border border-pink-300  rounded-xl bg-[#ffe4ea] ml-1">
           <Image src="/p3.png" alt="pink shoes" width="500" height="500" />
           <span>$500</span>
         </div>
 
-        <div className="w-40 h-40 sm:w-80 sm:h-80 border border-pink-300  rounded-xl bg-[#fce9ed] ml-1">
+        <div className="w-40 h-40 sm:w-80 sm:h-80 border border-pink-300  rounded-xl bg-[#ffe4ea] ml-1">
           <Image src="/p4.png" alt="pink shoes" width="500" height="500" />
           <span>$500</span>
         </div>
 
-        <div className="w-40 h-40 sm:w-80 sm:h-80 border border-pink-300  rounded-xl bg-[#fce9ed] ml-1">
+        <div className="w-40 h-40 sm:w-80 sm:h-80 border border-pink-300  rounded-xl bg-[#ffe4ea] ml-1">
           <Image src="/p5.png" alt="pink shoes" width="500" height="500" />
           <span>$500</span>
         </div>
 
-        <div className="w-40 h-40 sm:w-80 sm:h-80 border border-pink-300  rounded-xl bg-[#fce9ed] ml-1">
+        <div className="w-40 h-40 sm:w-80 sm:h-80 border border-pink-300  rounded-xl bg-[#ffe4ea] ml-1">
           <Image src="/p6.png" alt="pink shoes" width="500" height="500" />
           <span>$500</span>
         </div>

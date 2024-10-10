0839d0fa352787d23475147502b6fb1375a2c35e
syeda <sbtshakeel61@gmail.com>
2024-10-10 14:45:38 +0500

    adding code

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Hero2() {
   return (
-    <div className="sm:flex py-20 flex-wrap min-h-screen min-w-full  bg-gradient-to-br to-[#90e0ef] via-[#1b4965] from-[#90e0ef] ">
+    <div className="sm:flex py-20 flex-wrap min-h-screen min-w-full bg-gradient-to-br to-[#90e0ef] via-[#1b4965] from-[#90e0ef] overflow-hidden">
       <div className=" space-y-9 p-4">
         <h1 className="sm:text-6xl text-5xl text-wrap font-bold ">
           SMARTER ACCESSORIES
@@ -22,7 +22,13 @@ export default function Hero2() {
         </button>
       </div>
       <div className="flex-shrink-0 flex-row justify-center flex mt-5 mb-5 ">
-        <Image src="/fone.png" alt="" width={400} height={300} className="animate-pulse hover:animate-spin ease-linear delay-500  transition-transform" />
+        <Image
+          src="/fone.png"
+          alt=""
+          width={400}
+          height={300}
+          className="animate-pulse hover:animate-spin ease-linear delay-500  transition-transform"
+        />
       </div>
       <button className="sm:hidden flex justify-center py-2 border-cyan-600 border rounded-br-full w-40  bg-gradient-to-tr to-cyan-600 via-cyan-600 from-slate-500 animate-pulse ml-1">
         Shop now &#8594;
